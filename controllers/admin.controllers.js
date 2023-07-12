0f30c1f7501e0dedc88711da7b37448e9378e5a4
Alejo calandra <alejocalandra18@gmail.com>
2023-07-12 20:59:35 -0300

    admin

--- controllers/admin.controllers.js
+++ controllers/admin.controllers.js
@@ -1,3 +1,4 @@
+const Productos = require('../model/producto-model');
 const Usuario = require('../model/usuario-model');
 
 const cargarUsuarios = async (req, res) => {
@@ -13,6 +14,93 @@ const cargarUsuarios = async (req, res) => {
 	}
 };
 
+const crearProducto = async (req, res) => {
+	try {
+		const producto = new Productos(req.body);
+
+		//guardar usuario en DB
+		await producto.save();
+
+		res.status(201).json({
+			msg: 'Producto Creado correctamente',
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			msg: 'por favor contactate con el adminstrador',
+		});
+	}
+};
+
+const cargarProductos = async (req, res) => {
+	try {
+		const productos = await Productos.find();
+
+		res.status(200).json({
+			ok: true,
+			productos,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			msg: 'por favor contactate con el adminstrador',
+		});
+	}
+};
+
+const editarProducto = async (req, res) => {
+	try {
+		const productoEditar = await Productos.findById(req.body._id);
+
+		if (!productoEditar) {
+			return res.status(404).json({
+				ok: false,
+				msg: 'No existe ningun producto con este ID',
+			});
+		}
+
+		await Productos.findByIdAndUpdate(req.body._id, req.body);
+		res.status(200).json({
+			ok: true,
+			msg: 'producto Editado',
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			msg: 'por favor contactate con el adminstrador',
+		});
+	}
+};
+
+const eliminarProducto = async (req, res) => {
+	try {
+		const productoEliminar = await Productos.findById(req.params.id);
+
+		if (!productoEliminar) {
+			return res.status(404).json({
+				ok: false,
+				msg: 'No existe ningun producto con este ID',
+			});
+		}
+
+		await Productos.findByIdAndDelete(req.params.id);
+
+		res.status(200).json({
+			ok: true,
+			msg: 'Producto Eliminado exitosamente',
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			msg: 'por favor contactate con el adminstrador',
+		});
+	}
+};
+
 module.exports = {
 	cargarUsuarios,
+	crearProducto,
+	cargarProductos,
+	editarProducto,
+	eliminarProducto,
 };
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,26 @@
 const express = require('express');
-const { cargarUsuarios } = require('../controllers/admin.controllers');
+const {
+	cargarUsuarios,
+	crearProducto,
+	cargarProductos,
+	editarProducto,
+	eliminarProducto,
+} = require('../controllers/admin.controllers');
 // const { check } = require('express-validator');
 
 const routerAdmin = express.Router();
 
 routerAdmin.get('/usuarios', cargarUsuarios);
+
+routerAdmin.post(
+	'/new',
+	/*aca van las validaciones de express validator (name,precio,descripcion)*/ crearProducto
+);
+
+routerAdmin.get('/productos', cargarProductos);
+
+routerAdmin.put('/editar', editarProducto);
+
+routerAdmin.delete('/eliminar/:id', eliminarProducto);
 
 module.exports = routerAdmin;
